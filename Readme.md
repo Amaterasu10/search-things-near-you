1332435e95f8512c5c938c7af1fc617fafc5458a
John hinrich <johnhinrichggalindo@gmail.com>
2021-07-23 18:34:02 +0800

    updated Readme.md, change the style to that the map is full always in fullscreen, and added controls on the map

--- Readme.md
+++ Readme.md
@@ -1,19 +1,53 @@
 # search-things-near-you
 
 ## About
-This is made using [Tomtom's "maps sdk for web V6"](https://api.tomtom.com/maps-sdk-for-web/6.x/6.14.0/documentation/dist/modules/Maps.html).
-I decided to use Tomtom's sdk because compared to Google's API, it doesn't require a credit card.
+This is made using [Tomtom's Maps SDK for Web (v6)](https://developer.tomtom.com/maps-sdk-web-js).
+
+I decided to use Tomtom's sdk since it doesn't require a credit card.
 
 ### Purpose: Search things( places || restaurants || businesses ) near your location.
 
 ### Current progress: 
 - [x] Research 
-    - [x] Look for resources
-    Resources found :
-        - google API : Maps javascript API / Places API / Maps Embed API
-        - manticoresearch
-        - tomtom
+ - [x] Look for resources
+  Resources found:
+   - ~~ google API : Maps javascript API / Places API / Maps Embed API ~~
+   - ~~ manticoresearch ~~
+   - tomtom
 
-    - [x] determine the best resources to implement
-    - [ ] learning how to implement
+ - [x] determine the best resources to implement
+ - [x] learning how to implement
 
+-[x] implementation
+
+### Quick start 
+
+1. Sign-up to [tomtom](https://developer.tomtom.com/) to get your free API.
+2. After acquiring your API key, go to index.js and place your API key inside this variable `const myKey = "<your-tomtom-API-Key>";` 
+3. You can add more map controller by using the `map.adController()` function
+ex.
+
+`
+    var scale = new tt.ScaleControl({
+        maxWidth: 80,
+        unit: 'metric'
+    });
+    map.addControl(scale);
+`
+
+or
+
+`
+    map.addControl(new tt.FullscreenControl({
+        container: document.querySelector('body')
+    }));
+
+`
+
+or 
+
+`
+    var nav = new tt.NavigationControl({});
+    map.addControl(nav, 'bottom-right');
+
+`
--- index.js
+++ index.js
@@ -1,23 +1,197 @@
-const myKey = "PPHy2r3we6c0CYPT7w75ujdrrxZcaQ0X";
+const myKey = "<your-tomtom-API-Key>";
+let searchThis = "";
 
 var map = tt.map({
     key: myKey,
     container: "mapContainer"
 });
 
+
+
+// map controllers
+var scale = new tt.ScaleControl({
+    maxWidth: 80,
+    unit: 'metric'
+});
+  map.addControl(scale);
+
+
+map.addControl(new tt.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+ }));
+
+map.addControl(new tt.FullscreenControl({
+    container: document.querySelector('body')
+}));
+
+var nav = new tt.NavigationControl({});
+map.addControl(nav, 'bottom-right');
+
+
 var options = {
+
+    units: 'kilometers',
+    zoom: 15,
     searchOptions: {
         key: myKey,
         language: 'en-GB',
-        limit: 5
+    },
+    labels: {
+        noResultsMessage: 'No results found.',
+        placeholder: 'What are you looking for?'
     },
     autocompleteOptions: {
         key: myKey,
-        language: 'en-GB'
-    }
+        language: 'en-GB',
+        // query: searchThis
+    },
+    scrollZoom: {
+        around: 'center'
+    },
+    
 };
 
 var ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
 var searchBoxHTML = ttSearchBox.getSearchBoxHTML();
-//document.body.append();
-document.querySelector('.searchBox').append(searchBoxHTML)
+
+
+
+
+
+// var ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
+var searchMarkersManager = new SearchMarkersManager(map);
+ttSearchBox.on('tomtom.searchbox.resultsfound', handleResultsFound);
+ttSearchBox.on('tomtom.searchbox.resultselected', handleResultSelection);
+ttSearchBox.on('tomtom.searchbox.resultfocused', handleResultSelection);
+ttSearchBox.on('tomtom.searchbox.resultscleared', handleResultClearing);
+map.addControl(ttSearchBox, 'top-left');
+
+
+
+function handleResultsFound(event) {
+    var results = event.data.results.fuzzySearch.results;
+
+    if (results.length === 0) {
+        searchMarkersManager.clear();
+    }
+    searchMarkersManager.draw(results);
+    fitToViewport(results);
+}
+
+function handleResultSelection(event) {
+    var result = event.data.result;
+    if (result.type === 'category' || result.type === 'brand') {
+        return;
+    }
+    searchMarkersManager.draw([result]);
+    fitToViewport(result);
+}
+
+function fitToViewport(markerData) {
+    if (!markerData || markerData instanceof Array && !markerData.length) {
+        return;
+    }
+    var bounds = new tt.LngLatBounds();
+    if (markerData instanceof Array) {
+        markerData.forEach(function (marker) {
+            bounds.extend(getBounds(marker));
+        });
+    } else {
+        bounds.extend(getBounds(markerData));
+    }
+    map.fitBounds(bounds, { padding: 100, linear: true });
+}
+
+function getBounds(data) {
+    var btmRight;
+    var topLeft;
+    if (data.viewport) {
+        btmRight = [data.viewport.btmRightPoint.lng, data.viewport.btmRightPoint.lat];
+        topLeft = [data.viewport.topLeftPoint.lng, data.viewport.topLeftPoint.lat];
+    }
+    return [btmRight, topLeft];
+}
+
+function handleResultClearing() {
+    searchMarkersManager.clear();
+}
+
+
+
+function SearchMarkersManager(map, options) {
+    this.map = map;
+    this._options = options || {};
+    this._poiList = undefined;
+    this.markers = {};
+}
+
+SearchMarkersManager.prototype.draw = function (poiList) {
+    this._poiList = poiList;
+    this.clear();
+    this._poiList.forEach(function (poi) {
+        var markerId = poi.id;
+        var poiOpts = {
+            name: poi.poi ? poi.poi.name : undefined,
+            address: poi.address ? poi.address.freeformAddress : '',
+            distance: poi.dist,
+            classification: poi.poi ? poi.poi.classifications[0].code : undefined,
+            position: poi.position,
+            entryPoints: poi.entryPoints
+        };
+        var marker = new SearchMarker(poiOpts, this._options);
+        marker.addTo(this.map);
+        this.markers[markerId] = marker;
+    }, this);
+};
+
+SearchMarkersManager.prototype.clear = function () {
+    for (var markerId in this.markers) {
+        var marker = this.markers[markerId];
+        marker.remove();
+    }
+    this.markers = {};
+    this._lastClickedMarker = null;
+};
+
+
+
+function SearchMarker(poiData, options) {
+    this.poiData = poiData;
+    this.options = options || {};
+    this.marker = new tt.Marker({
+        element: this.createMarker(),
+        anchor: 'bottom'
+    });
+    var lon = this.poiData.position.lng || this.poiData.position.lon;
+    this.marker.setLngLat([
+        lon,
+        this.poiData.position.lat
+    ]);
+}
+
+SearchMarker.prototype.addTo = function (map) {
+    this.marker.addTo(map);
+    this._map = map;
+    return this;
+};
+
+SearchMarker.prototype.createMarker = function () {
+    var elem = document.createElement('div');
+    elem.className = 'tt-icon-marker-black tt-search-marker';
+    if (this.options.markerClassName) {
+        elem.className += ' ' + this.options.markerClassName;
+    }
+    var innerElem = document.createElement('div');
+    innerElem.setAttribute('style', 'background: white; width: 10px; height: 10px; border-radius: 50%; border: 3px solid black;');
+
+    elem.appendChild(innerElem);
+    return elem;
+};
+
+SearchMarker.prototype.remove = function () {
+    this.marker.remove();
+    this._map = null;
+};
